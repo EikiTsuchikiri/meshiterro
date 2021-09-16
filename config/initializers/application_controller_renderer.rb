a67bfd2da171f47c7e3c3708ff320d776b13dcdd
EikiTsuchikiri <tsuchikiri.eiki.infratop@gmail.com>
2021-09-16 10:58:04 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3c46f9a4e2784750e5aee0aea26d9ab30eead83c712b68eca9a76b203a6e1386a56ca7b72e3315c8108ca14526321627dc2bd5d946d42a79e8a5c0a7d3231336'
